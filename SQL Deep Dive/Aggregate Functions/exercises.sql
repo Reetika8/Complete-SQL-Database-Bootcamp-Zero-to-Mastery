47eeb66209207c0fd71a3272b1efb972df8fe9d0
Reetika8 <85224457+Reetika8@users.noreply.github.com>
2021-06-02 15:19:23 +0530

    Update exercises.sql

--- SQL Deep Dive/Aggregate Functions/exercises.sql
+++ SQL Deep Dive/Aggregate Functions/exercises.sql
@@ -8,10 +8,12 @@
 
 -- Question 1: What is the average salary for the company?
 -- Table: Salaries
+63810.744836143706
 
 
 -- Question 2: What year was the youngest person born in the company?
 -- Table: employees
+1952-02-01
 
 
 -- BEFORE YOU START
@@ -23,6 +25,7 @@
 
 -- Question 1: How many towns are there in france?
 -- Table: Towns
+36684
 
 
 -- BEFORE YOU START
@@ -35,10 +38,13 @@
 -- Question 1: How many official languages are there?
 -- Table: countrylanguage
 
+
 -- Question 2: What is the average life expectancy in the world?
 -- Table: country
+66.48603611164265
 
 -- Question 3: What is the average population for cities in the netherlands?
 -- Table: city
+350468.223584211817
 
 
