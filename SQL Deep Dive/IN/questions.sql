a3472cb4c71da7148db6f5c269a0ffc845950c8e
Reetika8 <85224457+Reetika8@users.noreply.github.com>
2021-06-08 11:50:10 +0530

    Update questions.sql

--- SQL Deep Dive/IN/questions.sql
+++ SQL Deep Dive/IN/questions.sql
@@ -2,7 +2,10 @@
 * DB: Store
 * Table: orders
 * Question: How many orders were made by customer 7888, 1082, 12808, 9623
-*/
+*/select count(orderid) from "public"."orders"
+where customerid in (7888, 1082, 12808, 9623); 
+
+6
 
 SELECT * FROM orders;
 
@@ -11,6 +14,9 @@ SELECT * FROM orders;
 * DB: World
 * Table: city
 * Question: How many cities are in the district of Zuid-Holland, Noord-Brabant and Utrecht?
-*/
+*/ select count(district)from "public"."city"
+where district in ('Zuid-Holland', 'Noord-Brabant', 'Utrecht');
+
+12
 
 SELECT * FROM city;
